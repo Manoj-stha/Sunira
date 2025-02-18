607ff2c6ed8874a26969e851721396f8524f24f5
Sudan Khadka <sudankhadka345@gmail.com>
2025-02-18 11:08:27 +0545

    model setup completed

--- backend/base/models.py
+++ backend/base/models.py
@@ -25,3 +25,32 @@ class Address(models.Model):
     def __str__(self):
         return f"{self.provience} {self.district} {self.city} {self.street}"
     
+
+class Property(models.Model):
+    owner=models.ForeignKey(User,on_delete=models.CASCADE,related_name='properties')
+    Property_Type=models.ForeignKey(Property_Type,on_delete=models.SET_NULL,null=True)
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=20,decimal_places=2)
+    sq_feet=models.DecimalField(max_digits=12,decimal_places=2)
+    address=models.OneToOneField(Address,on_delete=models.CASCADE)
+    is_available=models.BooleanField(default=True)
+    bedroom=models.PositiveSmallIntegerField(default=1)
+    bathroom=models.PositiveSmallIntegerField(default=1)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+class Listing(models.Model):
+    property=models.OneToOneField(Property, on_delete=models.CASCADE) 
+    listed_at=models.DateTimeField(auto_now_add=True)
+    is_sold=models.BooleanField(default=False)
+    is_feateured=models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"Listing for {self.Property.title}"
+    
+class Property_Image(models.Model):
+    property=models.ForeignKey(Property,on_delete=models.CASCADE,related_name="images")
+    image=models.ImageField(upload_to="property_images/")
--- backend/sunirabackend/settings.py
+++ backend/sunirabackend/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "sunirabackend.wsgi.application"
+AUTH_USER_MODEL = 'base.User'
 
 
 # Database
@@ -120,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+import os
+MEDIA_URL='/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
