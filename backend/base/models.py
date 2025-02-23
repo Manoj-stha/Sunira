66be8cd90aecbf9383763ff8e2ef9c3f2cf5983c
Sudan Khadka <sudankhadka345@gmail.com>
2025-02-23 12:15:49 +0545

    blogs and search done

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -53,3 +54,29 @@ class Listing(models.Model):
 class Property_Image(models.Model):
     property=models.ForeignKey(Property,on_delete=models.CASCADE,related_name="images")
     image=models.ImageField(upload_to="property_images/")
+    
+    
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+    
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True) 
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts') 
+    
+   
+    
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['publish'])
+        ]
+    
+    def __str__(self):
+        return self.title
